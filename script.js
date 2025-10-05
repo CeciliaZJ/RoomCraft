519ffd0f51d1e49d7858e23001106aff357f2046
cece <ceciliajiazhuo@gmail.com>
2025-10-04 20:32:52 -0400

    allows button to navigate to login

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.log("RoomCraft website loaded!");
 });
+
+//const myButton = document.getElementById('login');
+//document.getEventListener("login").onclick = function () {
+ //       window.location.href = "login.html"; }
